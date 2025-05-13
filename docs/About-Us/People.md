360126d84ce4979771f332f0e9b603d302727502
seirang <116618620+99JIK@users.noreply.github.com>
2025-05-14 02:04:23 +0900

    Create Skeleton & Initial commit

---
+++ docs/About-Us/People.md
@@ -0,0 +1,96 @@
+# People
+
+## 지도 교수
+
+
+## Full time
+
+### 박사 과정
+
+- 김덕엽 : [ejrduq77@naver.com](mailto:ejrduq77@naver.com)
+
+### 석사 과정
+
+- 김정인 : [99junginkim@gmail.com](mailto:99junginkim@gmail.com)
+
+## Part time
+
+### 박사 과정
+- 김계선 : [gic@knu.ac.kr](mailto:gic@knu.ac.kr)
+- 조성래 : [srcho@dgist.ac.kr](mailto:srcho@dgist.ac.kr)
+- 김호윤 : [hykim337@naver.com](mailto:hykim337@naver.com)
+- 강현우 : [hsdevils@gmail.com](mailto:hsdevils@gmail.com)
+- 임정현 : [jeonghyun.lim@lge.com](mailto:jeonghyun.lim@lge.com)
+- 장원석 : [cws07149@gmail.com](mailto:cws07149@gmail.com)
+- 이성희 : [lee3229910@gmail.com](mailto:lee3229910@gmail.com)
+- 정승모 : [seungmo1025@knu.ac.kr](mailto:seungmo1025@knu.ac.kr)
+- 이재훈 : [atheros1004@naver.com](mailto:atheros1004@naver.com)
+- 서정민 : [baal1004@naver.com](mailto:baal1004@naver.com)
+### 석사 과정
+- 맹한민 : [mhanmin@gmail.com](mailto:mhanmin@gmail.com)
+- 김성훈 : [uszzang@nate.com](mailto:uszzang@nate.com)
+- 배준철 : [jjoon@dip.or.kr](mailto:jjoon@dip.or.kr)
+- 박정태 : [nadia2013@naver.com](mailto:nadia2013@naver.com)
+- 박흥준 : [bbakeung@naver.com](mailto:bbakeung@naver.com)
+- 여창섭 : [ycs6841@givet.re.kr](mailto:ycs6841@givet.re.kr)
+- 김성하 : [sunghakim@gmail.com](mailto:sunghakim@gmail.com)
+- 김정권 : [1993kjk@gmail.com](mailto:1993kjk@gmail.com)
+- 김백진 : [bjkim@daooldns.co.kr](mailto:bjkim@daooldns.co.kr)
+---
+## 졸업생
+
+### 박사
+- 류동현 (ETRI) : [dhryu@etri.re.kr](mailto:dhryu@etri.re.kr)
+- 신영술 (경북대학교 복합지능ICT연구센터) : [youngsulshin@gmail.com](mailto:youngsulshin@gmail.com)
+- 류호동 (LG전자) : [hodong@gmail.com](mailto:hodong@gmail.com)
+- 백태산 (백교수 코딩스쿨) : [sans79@gmail.com](mailto:sans79@gmail.com)
+- Ajay Kumar Jha (North Dakota State Univ, USA) : [ajaykjha123@yahoo.com](mailto:ajaykjha123@yahoo.com)
+- Hossain Muhammad Iqbal (Brac Univ, Bangladesh) : [milon_mi7@yahoo.com](mailto:milon_mi7@yahoo.com)
+- 정수용 (한화시스템) : [kyo1363@naver.com](mailto:kyo1363@naver.com)
+- 서강복 : [dating1227@gmail.com](mailto:dating1227@gmail.com)
+### 석사
+- 김종형 (삼성전자) : [daegari.kim@samsung.com](mailto:daegari.kim@samsung.com)
+- 유민식 (LIG넥스원) : [msyoos123@nex1.co.kr](mailto:msyoos123@nex1.co.kr)
+- 박영준 (LG전자) : [koroot@gmail.com](mailto:koroot@gmail.com)
+- 김호준 (삼성전자) : [hojun00.kim@samsung.com](mailto:hojun00.kim@samsung.com)
+- Guo Hui : [ghelic@gmail.com](mailto:ghelic@gmail.com)
+- 박인수 (삼성전자) : [ispark82@gmail.com](mailto:ispark82@gmail.com)
+- 김두욱 (삼성전자) : [doowook.kim@samsung.com](mailto:doowook.kim@samsung.com)
+- 이병용 (LG전자) : [lby@lge.com](mailto:lby@lge.com)
+- 이정선 (LG CNS) : [rubyindark@knu.ac.kr](mailto:rubyindark@knu.ac.kr)
+- 석미희 (LG전자) : [miyachan84@gmail.com](mailto:miyachan84@gmail.com)
+- 권진욱 (현대모비스) : [greaper.kwon@mobis.co.kr](mailto:greaper.kwon@mobis.co.kr)
+- 조미정 (삼성전자) : [mijeong.cho@samsung.com](mailto:mijeong.cho@samsung.com)
+- Cao Thi Ly : [caoly1980@yahoo.com](mailto:caoly1980@yahoo.com)
+- 변지은 (삼성전자) : [fiaa20@hanmail.net](mailto:fiaa20@hanmail.net)
+- 박환석 (KT DS) : [alwaysgo@naver.com](mailto:alwaysgo@naver.com)
+- 김 해 (녹스엔터테인먼트) : [summerlife45@gmail.com](mailto:summerlife45@gmail.com)
+- 백수현 : [suhyun-99@nate.com](mailto:suhyun-99@nate.com)
+- 유지혜 : [ryujh@knu.ac.kr](mailto:ryujh@knu.ac.kr)
+- 김혁래 (삼성테크윈) : [gulekim@naver.com](mailto:gulekim@naver.com)
+- 장수영 (LG전자) : [ssf321@naver.com](mailto:ssf321@naver.com)
+- 이준범 (삼성탈레스): [junbum84.lee@samsung.com](mailto:junbum84.lee@samsung.com)
+- 우주형 (LG전자): [juhyung.woo@lge.com](mailto:juhyung.woo@lge.com)
+- 이승민 : [seung.min.lee.k@gmail.com](mailto:seung.min.lee.k@gmail.com)
+- 이학래 : [hakrae.lee@lge.com](mailto:hakrae.lee@lge.com)
+- 정미나 (CEST) : [jmn0220@naver.com](mailto:jmn0220@naver.com)
+- 장영근 (LG전자) : [youngkeun.jang@lge.com](mailto:youngkeun.jang@lge.com)
+- 황병일 : [halove2002@gmail.com](mailto:halove2002@gmail.com)
+- 공현우 (LG전자) : [hyunwoo.kong@lge.com](mailto:hyunwoo.kong@lge.com)
+- 곽용섭 (현대오토에버) : [iddqt@naver.com](mailto:iddqt@naver.com)
+- 서성화 : [seonghwa0509@gmail.com](mailto:seonghwa0509@gmail.com)
+- 박태광 : [kopi2642@naver.com](mailto:kopi2642@naver.com)
+- 서보영 (LG전자) : [sby4529@gmail.com](mailto:sby4529@gmail.com)
+- 윤보람 (무지개연구소) : [yunstar1105@naver.com](mailto:yunstar1105@naver.com)
+- 강혁명 (LG전자) : [palpati0516@gmail.com](mailto:palpati0516@gmail.com)
+- 조규상 (LG전자) : [joks1000@gmail.com](mailto:joks1000@gmail.com)
+- Modestus : [vincent.nkembe16@gmail.com](mailto:vincent.nkembe16@gmail.com)
+- 변지현 (LG전자) : [lge.byun@lge.com](mailto:lge.byun@lge.com)
+- 박철우 : [fisherest408@naver.com](mailto:fisherest408@naver.com)
+- 이기훈 : [naponrapon@naver.com](mailto:naponrapon@naver.com)
+- 최유정 : [yujeong1104@gmail.com](mailto:yujeong1104@gmail.com)
+- 김상태 : -
+- 진의정 (LG전자) : [atenboro7@naver.com](mailto:atenboro7@naver.com)
+- 김지훈 (LG전자) : [vvovvlplus@naver.com](mailto:vvovvlplus@naver.com)
+- 박민규 (LG전자) : [min9fore@gmail.com](mailto:min9fore@gmail.com)
+- 박세찬 : [hasmi5452@gmail.com](mailto:hasmi5452@gmail.com)
---
+++ docusaurus.config.js
@@ -0,0 +1,255 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Software Testing Lab.',
+  tagline: '경북대학교 소프트웨어 테스팅 연구실',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-site.example.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'ko',
+    locales: ['ko','en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: false, 
+        // {
+        //   showReadingTime: true,
+        //   feedOptions: {
+        //     type: ['rss', 'atom'],
+        //     xslt: true,
+        //   },
+        //   // Please change this to your repo.
+        //   // Remove this to remove the "edit this page" links.
+        //   editUrl:
+        //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        //   // Useful options to enforce blogging best practices
+        //   onInlineTags: 'warn',
+        //   onInlineAuthors: 'warn',
+        //   onUntruncatedBlogPosts: 'warn',
+        // },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+  plugins: [
+    [
+      '@docusaurus/plugin-content-blog',
+      {
+        id:'Domestic-Conference',
+        routeBasePath:'Domestic-Conference',
+        path:'./list/domestic-conference',
+        blogTitle:'국내 학술대회',
+        blogDescription:'국내 학술대회 관련',
+        blogSidebarCount:5,
+        postsPerPage:10,
+        feedOptions:{
+          type:['rss','atom'],
+          title:'',
+          copyright: `Copyright © ${new Date().getFullYear()} KNU ESELAB.`,
+        },
+      },
+    ],
+    [
+      '@docusaurus/plugin-content-blog',
+      {
+        id:'Internatinal-Conference',
+        routeBasePath:'International-Conference',
+        path:'./list/international-Conference',
+        blogTitle:'국제 학술대회',
+        blogDescription:'국제 학술대회 관련',
+        blogSidebarCount:5,
+        postsPerPage:10,
+        feedOptions:{
+          type:['rss','atom'],
+          title:'',
+          copyright: `Copyright © ${new Date().getFullYear()} KNU ESELAB.`,
+        },
+      },
+    ],
+    [
+      '@docusaurus/plugin-content-blog',
+      {
+        id:'Domestic-Journal',
+        routeBasePath:'Domestic-Journal',
+        path:'./list/domestic-journal',
+        blogTitle:'국내 저널',
+        blogDescription:'국내 저널 관련',
+        blogSidebarCount:5,
+        postsPerPage:10,
+        feedOptions:{
+          type:['rss','atom'],
+          title:'',
+          copyright: `Copyright © ${new Date().getFullYear()} KNU ESELAB.`,
+        },
+      },
+    ],
+    [
+      '@docusaurus/plugin-content-blog',
+      {
+        id:'International-Journal',
+        routeBasePath:'International-Journal',
+        path:'./list/international-journal',
+        blogTitle:'국제 저널',
+        blogDescription:'국제 저널 관련',
+        blogSidebarCount:5,
+        postsPerPage:10,
+        feedOptions:{
+          type:['rss','atom'],
+          title:'',
+          copyright: `Copyright © ${new Date().getFullYear()} KNU ESELAB.`,
+        },
+      },
+    ],
+    [
+      '@docusaurus/plugin-content-blog',
+      {
+        id:'Patent',
+        routeBasePath:'Patent',
+        path:'./list/patent',
+        blogTitle:'특허',
+        blogDescription:'특허 관련',
+        blogSidebarCount:5,
+        postsPerPage:10,
+        feedOptions:{
+          type:['rss','atom'],
+          title:'',
+          copyright: `Copyright © ${new Date().getFullYear()} KNU ESELAB.`,
+        },
+      },
+    ],
+    [
+      '@docusaurus/plugin-content-blog',
+      {
+        id:'Course',
+        routeBasePath:'Course',
+        path:'./list/course',
+        blogTitle:'강의',
+        blogDescription:'강의 관련',
+        blogSidebarCount:5,
+        postsPerPage:10,
+        feedOptions:{
+          type:['rss','atom'],
+          title:'',
+          copyright: `Copyright © ${new Date().getFullYear()} KNU ESELAB.`,
+        },
+      },
+    ],
+  ],
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'My Site',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Tutorial',
+          },
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/facebook/docusaurus',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
+                label: 'X',
+                href: 'https://x.com/docusaurus',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} KNU ESELAB.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
