d634fea4e0b9952690a3f7ba0fb193785a77f0be
99jik <99junginkim@gmail.com>
2025-05-26 16:43:41 +0900

    modified:   docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,14 +15,14 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://github.com/KNU-STLAB',
+  url: 'https://KNU-STLAB.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/SITE/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'STLAB', // Usually your GitHub org/user name.
+  organizationName: 'KNU-STLAB', // Usually your GitHub org/user name.
   projectName: 'SITE', // Usually your repo name.
 
   onBrokenLinks: 'throw',
