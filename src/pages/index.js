d3202fdac81baa494924740db3bb2698881c56a5
99jik <99junginkim@gmail.com>
2025-05-28 18:12:17 +0900

    Done

--- src/pages/index.js
+++ src/pages/index.js
@@ -48,7 +48,7 @@ function HomepageHeader() {
         <div className={clsx(styles.buttons, 'animate-on-scroll', styles.delay3)}>
           <Link
             className={clsx('button button--lg', styles.heroButtonPrimary)}
-            to="/STLAB">
+            to="/STLAB/About-Us">
             랩 소개 보기
           </Link>
         </div>
